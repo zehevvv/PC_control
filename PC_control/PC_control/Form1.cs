51598fd0a76179551322bb581c2cea222df35c44
zehevvv <43274651+zehevvv@users.noreply.github.com>
2022-10-26 16:29:17 +0300

    Add usp client keep alive

--- PC_control/PC_control/Form1.cs
+++ PC_control/PC_control/Form1.cs
@@ -2,8 +2,11 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,9 +15,120 @@ namespace PC_control
 {
     public partial class Form1 : Form
     {
+        private UdpClient m_udpClient = new UdpClient();
+        private int m_Port = 9001;
+        private string m_server_name = "orangepizero2";
+        private KeepAlive m_keepalive;
+
         public Form1()
         {
-            InitializeComponent();
+            InitializeComponent();            
+            m_udpClient.Connect(m_server_name, m_Port);
+            m_keepalive = new KeepAlive(m_server_name, m_Port, Server_alive, Server_dead);
         }
-    }
+
+        ~Form1()
+        {
+            m_udpClient.Close();
+        }
+
+        private void Server_alive()
+        {
+            Console.WriteLine("Server alive");
+        }
+
+        private void Server_dead()
+        {
+            Console.WriteLine("Server dead");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+
+            // This constructor arbitrarily assigns the local port number.
+            stopwatch.Start();
+
+            try
+            {
+                // Sends a message to the host to which you have connected.
+                Byte[] sendBytes = Encoding.ASCII.GetBytes("Is anybody there?");
+                m_udpClient.Send(sendBytes, sendBytes.Length);
+
+                //IPEndPoint object will allow us to read datagrams sent from any source.
+                IPEndPoint RemoteIpEndPoint = new IPEndPoint(IPAddress.Any, 0);
+
+                // Blocks until a message returns on this socket from a remote host.
+                Byte[] receiveBytes = m_udpClient.Receive(ref RemoteIpEndPoint);
+                string returnData = Encoding.ASCII.GetString(receiveBytes);
+
+                // Uses the IPEndPoint object to determine which of these two hosts responded.
+                Console.WriteLine("This is the message you received " +returnData.ToString());
+                Console.WriteLine("This message was sent from " + RemoteIpEndPoint.Address.ToString() + " on their port number " + RemoteIpEndPoint.Port.ToString());
+            }
+            catch (Exception e1)
+            {
+                Console.WriteLine(e1.ToString());
+            }
+            stopwatch.Stop();
+
+            Console.WriteLine("Elapsed Time is {0} ms", stopwatch.ElapsedMilliseconds);
+
+            //    try
+            //    {
+            //        String server = "orangepizero2";
+            //        String message = "hello";
+
+
+            //        // Create a TcpClient.
+            //        // Note, for this client to work you need to have a TcpServer
+            //        // connected to the same address as specified by the server, port
+            //        // combination.
+            //        Int32 port = 9001;
+
+            //        // Prefer using declaration to ensure the instance is Disposed later.
+            //        TcpClient client = new TcpClient(server, port);
+
+            //        // Translate the passed message into ASCII and store it as a Byte array.
+            //        Byte[] data = System.Text.Encoding.ASCII.GetBytes(message);
+
+            //        // Get a client stream for reading and writing.
+            //        NetworkStream stream = client.GetStream();
+
+            //        // Send the message to the connected TcpServer.
+            //        stream.Write(data, 0, data.Length);
+
+            //        Console.WriteLine("Sent: {0}", message);
+
+            //        // Receive the server response.
+
+            //        // Buffer to store the response bytes.
+            //        data = new Byte[256];
+
+            //        // String to store the response ASCII representation.
+            //        String responseData = String.Empty;
+
+            //        // Read the first batch of the TcpServer response bytes.
+            //        Int32 bytes = stream.Read(data, 0, data.Length);
+            //        responseData = System.Text.Encoding.ASCII.GetString(data, 0, bytes);
+            //        Console.WriteLine("Received: {0}", responseData);
+
+            //        // Explicit close is not necessary since TcpClient.Dispose() will be
+            //        // called automatically.
+            //        // stream.Close();
+            //        // client.Close();
+            //    }
+            //    catch (ArgumentNullException e1)
+            //    {
+            //        Console.WriteLine("ArgumentNullException: {0}", e1);
+            //    }
+            //    catch (SocketException e2)
+            //    {
+            //        Console.WriteLine("SocketException: {0}", e2);
+            //    }
+
+            //    Console.WriteLine("\n Press Enter to continue...");
+            //    Console.Read();
+        }
+        }
 }
---
+++ PC_control/PC_control/KeepAlive.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace PC_control
+{
+    public delegate void Keep_alive_method();    
+
+    class KeepAlive
+    {
+        private UdpClient m_udp_client;
+        private int m_port;
+        private string m_server_name;
+        private bool m_start = false;
+        private Thread m_thread;
+
+
+        public KeepAlive(string server_name, int port, Keep_alive_method alive_callback, Keep_alive_method not_alive_callback)
+        {            
+            m_port = port;
+            m_server_name = server_name;            
+            //m_udp_client = new UdpClient(m_server_name, m_port);
+            //m_udp_client.Client.ReceiveTimeout = 400;
+            m_thread = new Thread(() => Check_server_alive(server_name, port, alive_callback, not_alive_callback));
+            m_thread.Start();
+        }
+
+        private static void Check_server_alive(string server_name, int port, Keep_alive_method alive_event, Keep_alive_method not_alive_event)
+        {
+            bool is_alive = false;
+            int num_connect_failed = 0;
+            UdpClient udp_client = new UdpClient();
+            udp_client.Client.ReceiveTimeout = 200;       
+            
+            while (true)
+            {
+                try
+                {
+                    udp_client.Connect(server_name, port);
+
+
+                    // Sends a message to the host to which you have connected.
+                    Byte[] send_bytes = Encoding.ASCII.GetBytes("1");
+                    udp_client.Send(send_bytes, send_bytes.Length);
+
+                    //IPEndPoint object will allow us to read datagrams sent from any source.
+                    IPEndPoint remote_ip = new IPEndPoint(IPAddress.Any, 0);
+
+                    // Blocks until a message returns on this socket from a remote host.
+                    Byte[] receive_bytes = udp_client.Receive(ref remote_ip);
+
+                    if (receive_bytes.Length == 0 && is_alive)
+                    {
+                        not_alive_event.Invoke();
+                        is_alive = false;
+                    }
+                    else if (receive_bytes.Length != 0 && Encoding.ASCII.GetString(receive_bytes).Contains("1") && !is_alive)
+                    {
+                        alive_event.Invoke();
+                        is_alive = true;
+                        num_connect_failed = 0;
+                    }
+
+                    //if (receive_bytes.Length == 0)
+                    //    Console.WriteLine("alive");
+                    //else
+                    //    Console.WriteLine("dead");                    
+
+                    // Uses the IPEndPoint object to determine which of these two hosts responded.
+                    //Console.WriteLine("This is the message you received " + returnData.ToString());
+                    //Console.WriteLine("This message was sent from " + RemoteIpEndPoint.Address.ToString() + " on their port number " + RemoteIpEndPoint.Port.ToString());
+                }
+                catch (Exception e1)
+                {                    
+                    if (is_alive)
+                    {                        
+                        num_connect_failed++;
+
+                        if (num_connect_failed == 3)
+                        {
+                            not_alive_event.Invoke();
+                            is_alive = false;
+                        }
+                    }
+
+                    Console.WriteLine(e1.Message);
+                }                
+                Thread.Sleep(300);
+            }
+        }
+    }
+}
